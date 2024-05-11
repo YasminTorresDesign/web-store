6cfb579b3507c7dea3fb912ce230a0376a878fe0
YasminTorresDeveloper <126827320+YasminTorresDeveloper@users.noreply.github.com>
2024-05-10 19:46:41 -0500

    Creadas las funcionalidaes crear y editar productos con la vista admin

--- src/main/java/sena/webstore/controller/ProductoController.java
+++ src/main/java/sena/webstore/controller/ProductoController.java
@@ -1,10 +1,14 @@
 package sena.webstore.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,7 +26,8 @@ public class ProductoController {
     private ProductoService productoService;
 
     @GetMapping("")
-    public String show() {
+    public String show(Model model) {
+        model.addAttribute("productos", productoService.findAll());
         return "productos/show";
     }
 
@@ -40,4 +45,22 @@ public class ProductoController {
         productoService.save(producto);
         return "redirect:/productos";
     }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+		Producto producto= new Producto();
+		Optional<Producto> optionalProducto=productoService.get(id);
+		producto= optionalProducto.get();
+		
+		LOGGER.info("Producto buscado: {}",producto);
+		model.addAttribute("producto", producto);
+		
+		return "productos/edit";
+	}
+
+    @PostMapping("/update")
+	public String update(Producto producto) {
+		productoService.update(producto);		
+		return "redirect:/productos";
+	}
 }
