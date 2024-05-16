d0d6595d4b3e6ddee7c9ab985fede72cda1e70de
YasminTorresDeveloper <126827320+YasminTorresDeveloper@users.noreply.github.com>
2024-05-15 21:28:51 -0500

    Metodo para generarel numero de la orden en ordenService

--- src/main/java/sena/webstore/service/IOrdenService.java
+++ src/main/java/sena/webstore/service/IOrdenService.java
@@ -1,8 +1,11 @@
 package sena.webstore.service;
 
+import java.util.List;
+
 import sena.webstore.model.Orden;
 
 public interface IOrdenService {
+    List<Orden> findAll();
     Orden save (Orden orden);
 
 }
