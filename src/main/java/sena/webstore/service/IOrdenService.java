4e24092ae7c95c6843140f8f9a390bc899e76356
YasminTorresDeveloper <126827320+YasminTorresDeveloper@users.noreply.github.com>
2024-05-20 23:43:57 -0500

    Funcionalidad de mostrar los detalles de la compra del usuario

--- src/main/java/sena/webstore/service/IOrdenService.java
+++ src/main/java/sena/webstore/service/IOrdenService.java
@@ -1,13 +1,14 @@
 package sena.webstore.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import sena.webstore.model.Orden;
 import sena.webstore.model.Usuario;
 
 public interface IOrdenService {
     List<Orden> findAll();
-    //Optional<Orden> findById(Integer id);
+    Optional<Orden> findById(Integer id);
     Orden save (Orden orden);
     String generarNumeroOrden();
     List<Orden> findByUsuario (Usuario usuario);
--- src/main/java/sena/webstore/service/OrdenServiceImpl.java
+++ src/main/java/sena/webstore/service/OrdenServiceImpl.java
@@ -2,6 +2,7 @@ package sena.webstore.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -61,9 +62,9 @@ public class OrdenServiceImpl implements IOrdenService{
 		return ordenRepository.findByUsuario(usuario);
 	}
 
-	// @Override
-	// public Optional<Orden> findById(Integer id) {
-	// 	return ordenRepository.findById(id);
-	// }
+	@Override
+	public Optional<Orden> findById(Integer id) {
+		return ordenRepository.findById(id);
+	}
 
 }
