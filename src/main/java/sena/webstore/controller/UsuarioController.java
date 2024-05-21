7d84038082bb5043c5ea21e446a56d1af900f9d9
YasminTorresDeveloper <126827320+YasminTorresDeveloper@users.noreply.github.com>
2024-05-20 23:54:11 -0500

    Funcionalidad cerrar sesion

--- src/main/java/sena/webstore/controller/UsuarioController.java
+++ src/main/java/sena/webstore/controller/UsuarioController.java
@@ -105,9 +105,9 @@ public class UsuarioController {
 		return "usuario/detallecompra";
 	}
 	
-	// @GetMapping("/cerrar")
-	// public String cerrarSesion( HttpSession session ) {
-	// 	session.removeAttribute("idusuario");
-	// 	return "redirect:/";
-	// }
+	@GetMapping("/cerrar")
+	public String cerrarSesion( HttpSession session ) {
+		session.removeAttribute("idusuario");
+		return "redirect:/";
+	}
 }
